fe4e63b820dee377dad924ede82a0b72ff6cd325
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 22:21:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-document.addEventListener("DOMContentLoaded")
+document.addEventListener("DOMContentLoaded", function(){
+  fetch(imgUrl)
+})
