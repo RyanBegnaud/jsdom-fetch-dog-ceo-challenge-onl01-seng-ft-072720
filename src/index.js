bb4f68edbc21b30c6ca6dc88bcbb13d2ec75df7f
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 22:44:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function allBreeds(){
 
 function addList(dog) {
   let dogs = document.getElementById("dog-breeds")
-  dog.forEach()
+  dog.forEach
   let ul = document.createElement("ul")
   ul.innerHTML = dogName
   dogs.appendChild(ul)
